efd2536e3d94b7a9357f9bd133be5c4b4b9b3034
Atul Joshy <80053501+AtulJoshyy@users.noreply.github.com>
2021-10-11 23:58:19 +0530

    Add files via upload

---
+++ MLH-LHD/Day 1/Movie_Recommender.py
@@ -0,0 +1,6 @@
+import random
+
+movies = ['GoodFellas', 'The Godfather: Part II', 'Reservoir Dogs', 'Taxi Driver', 'The Godfather', 'The Godfather: Part III', 'Heat', "Carlito's Way", 'The Usual Suspects', 'Full Metal Jacket', 'Casino', 'Donnie Brasco', 'The Silence of the Lambs', "One Flew Over the Cuckoo's Nest", 'Pulp Fiction', 'The Departed', 'The Untouchables', 'Apocalypse Now', 'Se7en', 'A Clockwork Orange', 'American History X', 'Venom: Let There Be Carnage', 'Free Guy', 'Venom', 'Snake Eyes: G.I. Joe Origins', 'Deathstroke: Knights & Dragons - The Movie', 'Cosmic Sin', 'The Addams Family 2', 'The Stronghold', 'The Last Warrior: Root of Evil', 'The Seven Deadly Sins: Cursed by Light', 'Dragon Fury', 'Shang-Chi and the Legend of the Ten Rings', 'Solitary', 'The Suicide Squad', 'Crazy Fist', 'Jungle Cruise', 'LEGO Star Wars Terrifying Tales', 'F9', 'Old', 'Catch the Bullet']
+
+movie= random.choice(movies)
+print(movie)
